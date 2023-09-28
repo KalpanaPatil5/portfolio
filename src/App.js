c63c0dd6ac5b11daec1efbc4903c27f8f139df59
Kalpana Patil <kalpana.patil@localville.in>
2023-09-29 02:21:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import Contact from "./routes/Contact";
 import Project from "./routes/Project";
 import { Route, Routes } from "react-router-dom";
 import TechStack from "./routes/TechStack";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <BrowserRouter basename="/portfolio">
-        <Routes>
-          <Route exact path="/portfolio" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/techStack" element={<TechStack />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/techStack" element={<TechStack />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
