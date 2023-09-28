807c3ea534197c493a7596526da995b9b726b4fa
Kalpana Patil <kalpana.patil@localville.in>
2023-09-29 00:52:00 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Contact from "./routes/Contact";
 import Project from "./routes/Project";
 import { Route, Routes } from "react-router-dom";
 import TechStack from "./routes/TechStack";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Routes>
-        {/* <Route path="/" element={<Home/>}/> - for local system*/}
-        <Route exact path="/portfolio" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/techStack" element={<TechStack />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </>
+    <div className="container">
+      <BrowserRouter basename="/portfolio">
+        <Routes>
+          <Route exact path="/portfolio" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/techStack" element={<TechStack />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
